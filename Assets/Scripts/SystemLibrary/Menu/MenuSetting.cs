1d6419d6a41d7a0aa419386ea06d5879aeb77f85
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2025-07-29 17:05:25 +0900

    Seki_Add
    Add_MenuSetting

--- Assets/Scripts/SystemLibrary/Menu/MenuSetting.cs
+++ Assets/Scripts/SystemLibrary/Menu/MenuSetting.cs
@@ -11,6 +11,12 @@ public class MenuSetting : MenuBase {
 
     public override async UniTask Open() {
         await base.Open();
+        while (true) { 
+            if(Input.GetKeyDown(KeyCode.Escape)) break;
+
+            await UniTask.DelayFrame(1);
+        }
+        await Close();
     }
 
     public override async UniTask Close() {
