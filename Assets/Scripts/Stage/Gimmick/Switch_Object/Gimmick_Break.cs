196e88a35bf11785920b753031aa50ac539136b4
o-takehiro <mcd2476024@stu.o-hara.ac.jp>
2025-08-01 10:50:49 +0900

    oorui_add_BreakObject

---
+++ Assets/Scripts/Stage/Gimmick/Switch_Object/Gimmick_Break.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gimmick_Break : GimmickBase, IDestroyable {
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    public override void Initialize() {
+        gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// 準備
+    /// </summary>
+    public override void SetUp() {
+        gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    /// <exception cref="System.NotImplementedException"></exception>
+    protected override void OnUpdate() {
+    }
+ 
+    
+    
+    /// <summary>
+    /// オブジェクトを非アクティブにする
+    /// </summary>
+    /// <exception cref="System.NotImplementedException"></exception>
+    public void DestroyGimmick() {
+        // 消す
+        gameObject.SetActive(false);
+    }
+
+}
