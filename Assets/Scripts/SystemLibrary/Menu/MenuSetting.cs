34f83f2a8adefd7db9615e589e25af12d212f11b
IchikawaRiku <mcd2476003@stu.o-hara.ac.jp>
2025-08-06 17:05:51 +0900

    a

--- Assets/Scripts/SystemLibrary/Menu/MenuSetting.cs
+++ Assets/Scripts/SystemLibrary/Menu/MenuSetting.cs
@@ -8,7 +8,6 @@ using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using TMPro.EditorUtilities;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
