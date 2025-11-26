c21549378cde9b41508fbf57756ae4f4e53b0259
o-takehiro <mcd2476024@stu.o-hara.ac.jp>
2025-11-26 16:34:05 +0900

    oorui
    Add_particle

--- Assets/Scripts/Stage/StageObject/goalObject.cs
+++ Assets/Scripts/Stage/StageObject/goalObject.cs
@@ -37,6 +37,7 @@ public class goalObject : GimmickBase {
         if (collision.gameObject.layer == 6) {
             isGoal = true;
             UniTask task = SoundManager.instance.PlaySE(7);
+            EffectManager.Instance.Play(2, gameObject.transform.position);
             EndGameReason(eEndReason.Clear);
 
         }
